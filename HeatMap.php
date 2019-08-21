e7465f7665081a4d41dfcaec88798d2785817352
e141674 <dking6@ameren.com>
2019-08-21 12:31:55 -0500

    Created basic interface for HeatMap class

--- HeatMap.php
+++ HeatMap.php
@@ -1,7 +1,41 @@
 <?php
+
 namespace HeatMap;
 
 class HeatMap {
+    private $data;
+    private $width;
+    private $height;
+    private $image = NULL;
+
+    public function __construct(array $data = array(), $width = 500, $height = 500) {
+        $this->data = $data;
+        $this->width = $width;
+        $this->height = $height;
+
+        $this->setupImage();
+        $this->calibrate();
+    }
+
+    public function getImage() { return $this->image(); }
+
+    public function setData($data) {}
+    public function setWidth($width) {}
+    public function setHeight($height) {}
+
+    public function addData($data) {}
+
+    public function saveAsImage($filename, $type = 'png') {}
+    public function writeToBrowser() {}
+
+    private function isValidArray() {}
+
+    private function setupImage() {
+        $this->image = imagecreatetruecolor($this->width, $this->height)
+                           or die("Unable to initialize new image.");
+    }
+
+    private function calibrate() {}
 }
 
 ?>
